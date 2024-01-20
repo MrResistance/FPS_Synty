63e423efac689a546367e57cf449b6ebdcc3cb0e
MrResistance <Paul1208Gush@Gmail.com>
2024-01-20 18:15:35 +0000

    Cleaned up detection script

--- Assets/Code/Zombies/AI/Detection.cs
+++ Assets/Code/Zombies/AI/Detection.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public class Detection : MonoBehaviour
 {
@@ -15,7 +14,6 @@ public class Detection : MonoBehaviour
             m_zombie.ZombieStateMachine.TransitionTo(m_zombie.ZombieStateMachine.chaseState);
         }
     }
-
 
     private void OnTriggerExit(Collider other)
     {
